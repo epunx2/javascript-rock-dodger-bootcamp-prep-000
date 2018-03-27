b8a24794dde0fb06fb699c84ece0166fd7c5b64c
Eric Chrobak <eric@ericchrobak.com>
2018-03-27 02:34:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ function positionToInteger(p) {
 }
 
 function start() {
-  window.addEventListener('keydown', moveDodger());
+  window.addEventListener('keydown', moveDodger(event));
 
   START.style.display = 'none';
 
